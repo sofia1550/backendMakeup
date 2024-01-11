8d150251f2bf90a9ab40d5501faab2c2a288f5db
sofia1550 <luccciuksofia@gmail.com>
2024-01-11 19:06:30 -0300

    d

--- config/configMercadoPago.js
+++ config/configMercadoPago.js
@@ -1,8 +1,7 @@
 const mercadopago = require("mercadopago");
 
 mercadopago.configure({
-
-    access_token: 'TEST-7462530077634788-051020-9cf4f8c38800be4081564d1dc1d97c55-218527457'
+    access_token: process.env.MERCADOPAGO_ACCESS_TOKEN
 });
 
 module.exports = mercadopago;
